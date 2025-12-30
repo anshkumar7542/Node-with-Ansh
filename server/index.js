857e0b385ee3f9cfc91facabdf4f7f7b3253fd5c
Ansh kumar <anshsingh7542037689@gmail.com>
2025-12-30 22:24:59 +0530

    “Real learning starts when your hands get dirty with Node.js.”

--- server/index.js
+++ server/index.js
@@ -1,16 +1,49 @@
+//  const http = require("http");
+//  const fs = require("fs");
+//  const url = require("url");
+//  const myServer = http.createServer((req, res)=>{
+//     const myurl = url.parse(req.url);
+//     console.log(myurl);
+//     fs.appendFile('log.txt',`${Date.now()}: ${req.url} New Req Received\n`,(err,data)=>{
+//         switch(req.url){
+//             case '/':res.end("HomePage");
+//             break
+//             case '/about':res.end("i am ansh");
+//             break
+//             default:res.end("404 not found");
+//         }
+
+//     });
+ 
+//  });
+// myServer.listen(8000,()=>console.log("Server Started!"));
+
+
+
+
  const http = require("http");
  const fs = require("fs");
+ const url = require("url");
  const myServer = http.createServer((req, res)=>{
-    fs.appendFile('log.txt',`${Date.now()}: ${req.url} New Req Received\n`,(err,data)=>{
+    const myurl = url.parse(req.url);
+    console.log(myurl);
+    fs.appendFile('log.txt',`${Date.now()}: ${req.method} ${req.url} New Req Received\n`,(err,data)=>{
         switch(req.url){
-            case '/':res.end("HomePage");
-            break
+            case '/':
+            if(req.method==="GET")res.end("HomePage");
+            break;
             case '/about':res.end("i am ansh");
-            break
+            break;
+            case"/signup":
+            if(req.method==="GET") res.end("this is my signup form page");
+            else if (req.method=== "POST"){
+                //DB query
+                res.end("Success");
+            }
             default:res.end("404 not found");
         }
 
     });
-
+ 
  });
 myServer.listen(8000,()=>console.log("Server Started!"));
